52b2f10f59730ce033a50e9980cdc72cbb38c09b
root <root@studiet-Satellite-Pro-A300.(none)>
2012-11-17 17:39:21 +0100

    Added wrapper for connecting to smugmug called PhpSmug.

---
+++ cron.php
@@ -0,0 +1,9 @@
+<?php
+// /*
+// This script is a cron job. It checks the Smugmug site and calls an XML-RPC function if there are any new
+// albums on the specified user's account.
+// */
+
+
+
+?>
