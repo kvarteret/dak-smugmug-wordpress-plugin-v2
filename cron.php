699a1901f3ed10dbb1e032bf46fcc18edb3c7428
unknown <Per@Per-TOSH.(none)>
2013-01-12 17:20:19 +0100

    First working test with code. Tests If there are new albums and posts to db.

--- cron.php
+++ cron.php
@@ -4,6 +4,71 @@
 // albums on the specified user's account.
 // */
 
+require_once "phpSmug/phpSmug.php";
+
+$smugObject = new phpSmug( "APIKey=7DlM62R4ONejovMu7zSGtdWHMoJbsKk9", "AppName=kvarteret/1.0 (http://www.perandersen.no)" );
+
+$smugObject->login( "EmailAddress=eksponeringsetaten@gmail.com", "Password=stjernesalen" );
+//$smugObject->enableCache();
+
+$albums = $smugObject->albums_get();
+
+$db_albums = array();
+
+//DEBUG var_dump($albums);
+
+
+//get all album id's from database
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=smugTemp', "root", "jur8ucib");
+		    foreach($dbh->query('SELECT id from albums') as $row) {
+		    	array_push($db_albums, $row);
+		}	
+	
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+
+//compare to albums in $albums array
+	foreach ($albums as $album) {
+    		 $isInDb = false;
+			foreach($db_albums as $db_album) {
+				if($db_album['id'] == $album['id']) {
+					$isInDb = true;
+				}
+			}
+			if(!$isInDb) {
+				addNewAlbum($album, $dbh);
+			}
+    	}		
+
+    $dbh = null;
+
+//Function for adding new album to the db and creating a draft in wp.
+function addNewAlbum($album, $dbh) {
+	
+	echo "called addNewAlbum";
+	var_dump($album['id']);
+	echo $album['id'];
+	//make the draft
+	
+	//if it succeded add the id to the database
+	
+	$statement = "INSERT INTO `smugTemp`.`albums` (`id`) VALUES ('" . $album['id'] . "')";
+	echo $statement;
+	try {
+		$sth = $dbh->prepare($statement);
+		$sth->execute();		
+	}
+	catch (PDOException $e) { 
+		print "Error!: " . $e->getMessage() . "<br/>";
+    	die();
+	}
+	
+}	
 
 
 ?>
